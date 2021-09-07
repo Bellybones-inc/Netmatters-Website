f7abaafaa166a6dd12f405130251b37df8706224
Wilber30 <wilbersears@gmail.com>
2021-09-07 14:16:29 +0100

    Added auto-prefixer.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function(cb) {
+  gulp.src('scss/styles.css')
+    .pipe(autoprefixer('ie 10', 'ie 11'))
+    .pipe(gulp.dest('build'));
+    cb();
+});
