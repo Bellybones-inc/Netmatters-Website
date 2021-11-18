73ed36a014c41455c91f3f9e0b847cb2fefd169b
Wilber30 <wilbersears@gmail.com>
2021-11-18 17:15:29 +0000

    - Added sidebar HTML.

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,15 @@
-const body = document.body;
-let lastScroll = 0;
-const header = document.querySelector('header');
 
-window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset
 
-    // if(currentScroll >= 0) {
-    //   header.classList.remove("scroll-up")
-    // }
+$(document).ready(function(){
+      $('.slide-inner').slick({
+        variableWidth: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        speed: 3000,
+      });
+    });
 
-    // This is causing the scroll-down to be added/ menu disappear
-    // if (currentScroll >  500 && !body.classList.contains("scroll-down")) {
-    //   header.classList.remove("scroll-up")
-    //   header.classList.add("scroll-down")
-    // }
-
-    if (currentScroll < lastScroll && !body.classList.contains("scroll-down")) {
-      header.classList.remove("scroll-down")
-      header.classList.add("scroll-up")
-    }
-
-    lastScroll = currentScroll;
-})
+const sidebar = document.getElementById("sidebar");
