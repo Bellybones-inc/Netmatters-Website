dd21dcce95c28dfa96013c8f4a64b626167cd7dd
Wilber30 <wilbersears@gmail.com>
2021-11-21 15:38:18 +0000

    - Adjusted sidebar navigation.

--- JS/main.js
+++ JS/main.js
@@ -16,11 +16,15 @@ $(document).ready(function(){
 const sidebar = document.querySelector(".sidebar");
 const hamburger = document.getElementById("hamburger");
 const bars = document.querySelector(".fas");
-const main = document.querySelector(".entire");
+const main = document.querySelector(".main");
 const header = document.getElementById("top_header");
+const inner = document.querySelector(".main-inner");
 
 hamburger.addEventListener('click', () => {
   bars.classList.toggle('in-use');
   sidebar.classList.toggle('is-active');
-  main.classList.toggle('shade');
+  main.classList.toggle('shift');
+  inner.classList.toggle('over');
 })
+
+// body.document.style.transition('all 0.8s ease 0.8s');
