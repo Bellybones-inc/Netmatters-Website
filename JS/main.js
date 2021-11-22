e8331e8144346ede6f71067fcf4ffe42b2d9eef5
Wilber30 <wilbersears@gmail.com>
2021-11-22 16:50:02 +0000

    - Applied transparent overlay, disabled scroll and menu animation.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,4 @@
-
-
-
+// Slick
 $(document).ready(function(){
       $('.slide-inner').slick({
         variableWidth: true,
@@ -13,18 +11,61 @@ $(document).ready(function(){
       });
     });
 
-const sidebar = document.querySelector(".sidebar");
+const sidebar = document.getElementById("sidebar");
 const hamburger = document.getElementById("hamburger");
 const bars = document.querySelector(".fas");
 const main = document.querySelector(".main");
 const header = document.getElementById("top_header");
 const inner = document.querySelector(".main-inner");
+const outer = document.querySelector(".main-outer");
+const body = document.getElementsByTagName('body')[0];
+
+// Burger bars
+const barTop = document.querySelector(".top");
+const barMiddle = document.querySelector(".middle");
+const barBottom = document.querySelector(".bottom");
 
 hamburger.addEventListener('click', () => {
-  bars.classList.toggle('in-use');
+  outer.classList.toggle('hidden');
+  barTop.classList.toggle('ro');
+  barMiddle.classList.toggle('tate');
+  barBottom.classList.toggle('out');
   sidebar.classList.toggle('is-active');
   main.classList.toggle('shift');
   inner.classList.toggle('over');
+  body.classList.toggle('nav_active');
 })
+
+// document.onclick = function(e) {
+//   if(e.target.id !== 'sidebar')
+//   {
+//       outer.classList.remove('hidden');
+//       bars.classList.remove('in-use');
+//       sidebar.classList.remove('is-active');
+//       main.classList.remove('shift');
+//       inner.classList.remove('over');
+//       body.classList.toggle('nav_active');
+//   }
+// }
+
+  sidebar.addEventListener('click', () => {
+  outer.classList.remove('hidden');
+  bars.classList.remove('in-use');
+  sidebar.classList.remove('is-active');
+  main.classList.remove('shift');
+  inner.classList.remove('over');
+  barTop.classList.remove('ro');
+  barMiddle.classList.remove('tate');
+  barBottom.classList.remove('out');
+})
+
+
+//   body.onclick = function() {
+//   outer.classList.remove('hidden');
+//   bars.classList.remove('in-use');
+//   sidebar.classList.remove('is-active');
+//   main.classList.remove('shift');
+//   inner.classList.remove('over');
+// })
 
 // body.document.style.transition('all 0.8s ease 0.8s');
