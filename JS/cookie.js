7c307abcb0eca0f8096e956155def6ac44bd3403
Wilber30 <wilbersears@gmail.com>
2021-11-24 12:49:49 +0000

    - Added simple JS program for cookie pop-up.

--- JS/cookie.js
+++ JS/cookie.js
@@ -1,27 +1,12 @@
-$('.qookies').qookies();
 
-$('.qookies').qookies({
-  theme:'theme-default',
-  position:'bottom-right',
-  expires: 30,// 30 days
-  path:'/',
-  value:'1',
-  cookieName:'qookies-accepted',
-  link: {
-    text: lang.link.text,
-    url: lang.link.url
-  },
-});
+const cookieSection = document.getElementById("cookie-consent");
+const acceptBtn = document.getElementById("accept");
 
-var lang= {
-    title:"Privacy Policy",
-    description:"<p> This website uses cookies to ensure you get the best experience on our website.  </p>",
-    link:{
-      text:"Read More",
-      url:"//www.jqueryscript.net/privacy/"
-    },
-    buttons:{
-      accept:"Got it!",
-      cancel:"Cancel"
-    }
+window.onload = () => {
+  cookieSection.style.display = "flex";
 };
+
+acceptBtn.addEventListener('click', () => {
+  cookieSection.style.display = "none";
+
+})
