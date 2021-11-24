c9f18fef25593e105edfda54f8ec4c5550663247
Wilber30 <wilbersears@gmail.com>
2021-11-24 16:20:45 +0000

    - Expanded on the original initial JS cookie program.

--- JS/cookie.js
+++ JS/cookie.js
@@ -1,12 +1,15 @@
 
+
 const cookieSection = document.getElementById("cookie-consent");
 const acceptBtn = document.getElementById("accept");
 
-window.onload = () => {
-  cookieSection.style.display = "flex";
-};
-
-acceptBtn.addEventListener('click', () => {
+ acceptBtn.onclick = () => {
+   localStorage.setItem('true', 'fine');
   cookieSection.style.display = "none";
+}
 
-})
+window.onload = () => {
+  if("true" in localStorage){
+    cookieSection.style.display = "none";
+  }
+}
