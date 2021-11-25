7fddab97c4a9d5d841db62722bd5d78220c27d77
Wilber30 <wilbersears@gmail.com>
2021-11-25 15:15:12 +0000

    - Resolved side Navigation issue.

--- JS/main.js
+++ JS/main.js
@@ -14,19 +14,20 @@ $(document).ready(function(){
 // slide-out menu variables
 const sidebar = document.getElementById("sidebar");
 const hamburger = document.getElementById("hamburger");
-const bars = document.querySelector(".fas");
 const main = document.querySelector(".main");
 const header = document.getElementById("top_header");
 const inner = document.querySelector(".main-inner");
 const outer = document.querySelector(".main-outer");
 const body = document.getElementsByTagName('body')[0];
+const pageParent = document.querySelector(".page-parent");
 
 // Burger bars
 const barTop = document.querySelector(".top");
 const barMiddle = document.querySelector(".middle");
 const barBottom = document.querySelector(".bottom");
 
-hamburger.onclick = () => {
+// Show side-menu, move page and add overlay
+hamburger.addEventListener("click", () => {
   outer.classList.toggle('hidden');
   barTop.classList.toggle('ro');
   barMiddle.classList.toggle('tate');
@@ -34,19 +35,19 @@ hamburger.onclick = () => {
   sidebar.classList.toggle('is-active');
   main.classList.toggle('shift');
   inner.classList.toggle('over');
-  body.classList.toggle('nav_active');
-}
+  pageParent.classList.toggle('layer');
+})
 
-sidebar.onclick = () => {
+// Removes side-menu and returns page to default. 
+pageParent.addEventListener("click", () => {
+  if(inner.classList.toggle('over')) {
   outer.classList.remove('hidden');
   barTop.classList.remove('ro');
   barMiddle.classList.remove('tate');
   barBottom.classList.remove('out');
-  // bars.classList.remove('in-use');
   sidebar.classList.remove('is-active');
   main.classList.remove('shift');
   inner.classList.remove('over');
-  barTop.classList.remove('ro');
-  barMiddle.classList.remove('tate');
-  barBottom.classList.remove('out');
-}
+  pageParent.classList.remove('layer');
+  }
+})
