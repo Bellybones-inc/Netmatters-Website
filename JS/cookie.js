ef3654a4ee15e39c688dea2f3009bee2234d736c
Wilber30 <wilbersears@gmail.com>
2021-12-07 17:51:16 +0000

    - Loads page content before displaying/ evaluating cookies.

--- JS/cookie.js
+++ JS/cookie.js
@@ -1,20 +1,18 @@
-
-
 const cookieSection = document.getElementById("cookie-consent");
 const acceptBtn = document.getElementById("accept");
 const disableScroll = document.querySelector(".main");
 
- acceptBtn.addEventListener("click", () => {
-   localStorage.setItem('true', 'fine');
-   cookieSection.style.display = "none";
-   disableScroll.classList.remove('scroll_stop');
+acceptBtn.addEventListener("click", () => {
+  localStorage.setItem('true', 'fine');
+  cookieSection.style.display = "none";
+  disableScroll.classList.remove('scroll_stop');
 })
 
-window.onload = () => {
-  if("true" in localStorage){
+document.addEventListener("DOMContentLoaded", function(event) {
+  if ("true" in localStorage) {
     cookieSection.style.display = "none";
     disableScroll.classList.remove('scroll_stop');
   } else {
     disableScroll.classList.add('scroll_stop');
   }
-}
+});
