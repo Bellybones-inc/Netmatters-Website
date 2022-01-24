60c744f11161a82ffc265a639ffdc73372cbca71
Wilber30 <wilbersears@gmail.com>
2022-01-24 17:19:21 +0000

    - Adds include links to contact page.

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
+<html lang="en">
 
+  <head>
 
-<?php
-include 'inc/header.php';
-?>
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1">
+
+    <title>Netmatters Homepage</title>
+
+    <!-- Slick -->
+    <link rel="stylesheet" type="text/css" href="scss/slick.css" />
+    <link rel="stylesheet" type="text/css" href="scss/slick-theme.css" />
+
+    <!-- CSS and Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="scss/all.min.css">
+
+    <link rel="stylesheet" type="text/css" href="scss/styles.css">
+
+    <script nomodule>
+      window.MSInputMethodContext && document.documentMode && document.write(
+        '<link rel="stylesheet" href="/css/bootstrap-ie11.min.css"><script src="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js,npm/ie11-custom-properties@4,npm/element-qsa-scope@1"><\/script><script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.min.js?features=default%2CNumber.parseInt%2CNumber.parseFloat%2CArray.prototype.find%2CArray.prototype.includes"><\/script>'
+      );
+    </script>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <?php
+    include 'inc/header.php';
+    ?>
 
               <!-- banner section start  -->
 
@@ -590,8 +624,6 @@ include 'inc/header.php';
 
                   <button name="submit" class="btn submit btn-primary">Subscribe</button>
 
-
-
                 </div> <!-- newsletter inner end -->
               </div> <!-- newsletter container end -->
             </div> <!-- newsletter_section end -->
@@ -608,7 +640,6 @@ include 'inc/header.php';
       <?php
       include 'inc/sidebar.php';
       ?>
-
 
     </div> <!-- main  -->
   </div> <!-- top container  -->
