9e457b81edc16433c8159c0dd82f9b8b7cb1a365
Wilber30 <wilbersears@gmail.com>
2022-01-24 18:46:32 +0000

    - Removes pages folder.

---
+++ contact.php
@@ -0,0 +1,20 @@
+
+    <?php
+    require 'inc/header.php';
+    ?>
+<form class="" action="index.html" method="post">
+  
+</form>
+
+
+
+    <?php
+    require 'inc/footer.php';
+    ?>
+
+    <?php
+    require 'inc/sidebar.php';
+    ?>
+
+</body>
+</html>
--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,39 @@
+<html lang="en">
 
+  <head>
+
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1">
+
+    <title>Netmatters Homepage</title>
+
+    <!-- Slick -->
+    <link rel="stylesheet" type="text/css" href="scss/slick.css" />
+    <link rel="stylesheet" type="text/css" href="scss/slick-theme.css" />
+
+    <!-- CSS and Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="scss/all.min.css">
+
+    <link rel="stylesheet" type="text/css" href="scss/styles.css">
+
+    <script nomodule>
+      window.MSInputMethodContext && document.documentMode && document.write(
+        '<link rel="stylesheet" href="/css/bootstrap-ie11.min.css"><script src="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js,npm/ie11-custom-properties@4,npm/element-qsa-scope@1"><\/script><script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.min.js?features=default%2CNumber.parseInt%2CNumber.parseFloat%2CArray.prototype.find%2CArray.prototype.includes"><\/script>'
+      );
+    </script>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  </head>
+  <body>
   <div id="top_container">
 
     <div class="main">
@@ -72,12 +107,12 @@
                             <div class="btn-group">
                               <div class="d-none d-md-none d-lg-inline-flex contact_btn">
                                 <form>
-                                <button formaction="pages/contact.php" "type="button" class="btn btn-primary contact">
+                                <button formaction="contact.php" "type="button" class="btn btn-primary contact">
                                   <i class="far fa-paper-plane"></i>
                                   contact
                                 </button>
                               </form>
-                              </div>
+                            </div>
                             </div>
 
                             <!-- Mini search form -->
