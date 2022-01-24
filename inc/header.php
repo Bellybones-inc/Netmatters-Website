1ef5fc78a3c99f92e75676997330eb3e851d9c34
Wilber30 <wilbersears@gmail.com>
2022-01-24 09:37:18 +0000

    - Copied header into PHP file.

---
+++ inc/header.php
@@ -0,0 +1,249 @@
+<div id="top_container">
+
+  <div class="main">
+    <div class="main-outer">
+      <div class="main-inner">
+
+        <div class="page-parent">
+          <div class="full-page">
+
+            <!-- Cookie pop-up -->
+
+            <div id="cookie-consent">
+
+              <div class="qookie_outer">
+
+                <div id="cookie-content">
+                  <div class="cookie_header">
+                    <h3>Cookies Policy</h3>
+                  </div>
+                  <div class="cookie_text">
+                    <p>Our website uses cookies. This helps us provide you with a good
+                      experience on our website. To see what cookies we use and what they do,
+                      and to opt-in on non-essential cookies click "change settings".
+                      For a detailed explanation, click on "<a href="#">Privacy Policy</a>"
+                      otherwise click "Accept Cookies" to enter.
+                    </p>
+                  </div>
+                  <div class="cookie_footer">
+                    <a id="settings" class="btn btn-primary">Change Settings</a>
+
+                    <a id="accept" class="btn btn-primary accept">Accept Cookies</a>
+                  </div>
+                </div>
+
+              </div>
+
+            </div> <!-- cookie -->
+
+            <!-- start of header  -->
+
+            <div class="top_header">
+
+              <header>
+
+                <div class="inner">
+
+                  <div class="header_container">
+                    <div class="header_row">
+                      <div class="logo_container">
+                        <img class="img-logo" src="img/netmatters-logo.png" alt="Netmatters company logo">
+                      </div>
+
+                      <div class="action_wrapper">
+                        <div class="action">
+
+                          <div class="d-md-none mobile_icon">
+                            <span class="telephone">
+                              <i class="fas fa-phone-alt"></i>
+                            </span>
+                          </div>
+
+                          <div class="btn-group">
+                            <div class="d-none d-md-none d-lg-inline-flex contact_btn">
+                              <button type="button" class="btn btn-primary support">
+                                <i class="fas fa-mouse"></i>
+                                support
+                              </button>
+                            </div>
+                          </div>
+
+                          <div class="btn-group">
+                            <div class="d-none d-md-none d-lg-inline-flex contact_btn">
+                              <button type="button" class="btn btn-primary contact">
+                                <i class="far fa-paper-plane"></i>
+                                contact
+                              </button>
+                            </div>
+                          </div>
+
+                          <!-- Mini search form -->
+
+                          <form class="vis_search" method="get" accept-charset="utf-8">
+                            <input id="mini" class="form-control mini" type="text" placeholder="Search..">
+                            <button class="search-submit" type="submit">
+                              <i class="fas fa-search"></i>
+                            </button>
+                          </form>
+
+                          <!-- menu -->
+
+                          <div id="hamburger">
+                            <div class="bar top"></div>
+                            <div class="bar middle"></div>
+                            <div class="bar bottom"></div>
+                            <p class="menu">menu</p>
+                          </div>
+
+                        </div> <!-- action -->
+                      </div> <!-- action_wrapper -->
+                    </div> <!-- header_row -->
+
+                    <div class="extra-space"></div>
+
+                    <!-- large search bar -->
+
+                    <form class="lg-search" action="#" method="GET">
+                      <input id="control" class="form-control search-bar d-md-none d-lg-none" placeholder="Search...">
+                      <button class="d-md-none" id="submit-lg" type="submit">
+                        <i id="search-lg" class="fas fa-search"></i>
+                      </button>
+                    </form>
+
+                  </div> <!-- container end  -->
+                </div> <!-- inner end  -->
+
+
+                <!-- navigation banner -->
+
+                <div class="main-navigation d-none d-lg-block">
+                  <div class="container navigation-container">
+
+                    <ul class="nav-list">
+                      <li class="web web-one">
+                        <a class="link_icn lin-web" href="#">
+                          <div class="con-wrapper">
+                            <i class="fas fa-code company-con con-1"></i>
+                          </div>
+                          <small class="sm-txt">Web</small><br>
+                          Design
+                        </a>
+                        <div class="options option-1">
+                          <ul class="nav-menu nav-web">
+                            <li class="nav-item"><a class="sub-link" href="#">Stylish Websites</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Ecommerce Stores</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Branding</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Apps</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Web Hosting</a></li>
+                            <li class="nav-item"><a class="sub-last" href="#">Pay Monthly Websites</a></li>
+                          </ul>
+                        </div>
+                      </li>
+
+                      <li class="web web-two">
+                        <a class="link_icn lin-sup" href="#">
+                          <div class="con-wrapper">
+                            <i class="fas fa-desktop company-con con-2"></i>
+                          </div>
+                          <small class="sm-txt">IT</small><br>
+                          Support
+                        </a>
+                        <div class="options option-2">
+                          <ul class="nav-menu nav-sup">
+                            <li class="nav-item"><a class="sub-link" href="#">Managed It</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Business It</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Office 365</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Consultancy</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Cloud Provider</a></li>
+                            <li class="nav-item"><a class="sub-last" href="#">Data Backup</a></li>
+                          </ul>
+                        </div>
+                      </li>
+
+                      <li class="web web-three">
+                        <a class="link_icn lin-serv" href="#">
+                          <div class="con-wrapper">
+                            <i class="fas fa-phone-alt company-con con-3"></i>
+                          </div>
+                          <small class="sm-txt">Telecoms</small><br>
+                          Services
+                        </a>
+                        <div class="options option-3">
+                          <ul class="nav-menu nav-serv">
+                            <li class="nav-item"><a class="sub-first" href="#">Gigabit Voucher</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Hosted Voip</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Business Voip</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Business Broadband</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Leased Line</a></li>
+                            <li class="nav-item"><a class="sub-last" href="#">3cx Systems</a></li>
+                          </ul>
+                        </div>
+                      </li>
+
+                      <li class="web web-four">
+                        <a class="link_icn lin-soft" href="#">
+                          <div class="con-wrapper">
+                            <i class="fas fa-th company-con con-4"></i>
+                          </div>
+                          <small class="sm-txt">Bespoke</small><br>
+                          Software
+                        </a>
+                        <div class="options option-4">
+                          <ul class="nav-menu nav-soft">
+                            <li class="nav-item"><a class="sub-link" href="#">Workflow</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Automation</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Integration</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Database</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Sharepoint</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Business Management</a></li>
+                            <li class="nav-item"><a class="sub-last navision" href="#">Navision</a></li>
+                          </ul>
+                        </div>
+                      </li>
+
+                      <li class="web web-five">
+                        <a class="link_icn lin-mark" href="#">
+                          <div class="con-wrapper">
+                            <i class="fas fa-chart-bar company-con con-5"></i>
+                          </div>
+                          <small class="sm-txt">Digital</small><br>
+                          Marketing
+                        </a>
+                        <div class="options option-5">
+                          <ul class="nav-menu nav-digi">
+                            <li class="nav-item"><a class="sub-link" href="#">Search (SEO)</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Paid (PPC)</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Conversion (CRO)</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Email</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Social Media</a></li>
+                            <li class="nav-item"><a class="sub-last" href="#">Content</a></li>
+                          </ul>
+                        </div>
+                      </li>
+
+                      <li class="web web-six">
+                        <a class="link_icn lin-sec" href="#">
+                          <div class="con-wrapper">
+                            <i class="fas fa-shield-alt company-con con-6"></i>
+                          </div>
+                          <small class="sm-txt">Cyber</small><br>
+                          Security
+                        </a>
+                        <div class="options option-6">
+                          <ul class="nav-menu nav-sec">
+                            <li class="nav-item"><a class="sub-link" href="#">Assessment</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Management</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Penetration Testing</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">Cyber Essentials</a></li>
+                            <li class="nav-item"><a class="sub-link" href="#">PCS/DSS</a></li>
+                            <li class="nav-item"><a class="sub-last" href="#">Hacker Prevention</a></li>
+                          </ul>
+                        </div>
+                      </li>
+
+                    </ul>
+
+                  </div>
+                </div> <!-- Navigation end -->
+              </header>
+            </div> <!-- top_header end  -->
