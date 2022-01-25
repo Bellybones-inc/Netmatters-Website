9cd38d7f378f48bda5f194832cc1ffc8c4a7cb7e
Wilber30 <wilbersears@gmail.com>
2022-01-25 11:08:51 +0000

    - Corrects closing div tag issue on contact page.

--- contact.php
+++ contact.php
@@ -3,7 +3,11 @@
     require 'inc/header.php';
     ?>
 
+  </div>
+  </div>
+
     <div class="container">
+
 
 
 
@@ -51,6 +55,20 @@
             <textarea class="form-control" name="message" rows="10" cols="50"></textarea>
           </div>
         </div>
+
+        <div class="marketing_info_container">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">
+          </div>
+          <p class="policy_text">Please tick this box if you wish to receive marketing
+            information from us. Please see our
+            <a href="#" class="privacy">Privacy Policy</a> for more
+            information on how we use your data.
+            <span class="space_between">&nbsp;</span>
+          </p>
+        </div>
+
+        <button type="submit" name="button">Send Enquiry</button>
 
       </div>
     </form>
