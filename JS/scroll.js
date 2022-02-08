5c791194e69d77e516f81363a3ab5a14fc4df8ed
Wilber30 <wilbersears@gmail.com>
2022-02-08 13:27:40 +0000

    - Removes code from scroll.js.

--- JS/scroll.js
+++ JS/scroll.js
@@ -7,17 +7,17 @@ let lastScroll = 0;
 window.addEventListener('scroll', () => {
   const currentScroll = window.pageYOffset
 
-// If the page is at the top
-  if(currentScroll <= 0) {
-    // headerTop.classList.remove("scroll-up");
-    slideContainer.style.marginTop = "0px";
-  }
+  // If the page is at the top
+    if(currentScroll <= 0) {
+      headerTop.classList.remove("scroll-up")
+      slideContainer.style.marginTop = "0px";
+    }
+    // This is causing the scroll-down to be added/ menu disappear
+    if (currentScroll > 1200 && !headerTop.classList.contains("scroll-down")) {
+      headerTop.classList.remove('scroll-up')
+      headerTop.classList.add('scroll-down')
+    }
 
-  // This is causing the scroll-down to be added/ menu disappear
-  if (currentScroll > 350 && !headerTop.classList.contains("scroll-down")) {
-    headerTop.classList.remove('scroll-up');
-    headerTop.classList.add('scroll-down');
-  }
 
   if (currentScroll < lastScroll && headerTop.classList.contains("scroll-down")) {
     headerTop.classList.remove('scroll-down');
